d044884b0a5a3dd4cad2e5fa246b263411b4089a
Josh Fletcher <jfletcher@nuxeo.com>
2015-06-03 10:25:04 -0700

    Parameterize reset.sh

--- bin/reset.sh
+++ bin/reset.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 
-# Script to manually remove a SNAPTHOT Studio package and re-install it. Useful when an update fails.
+# Script to manually remove a SNAPSHOT Studio package and re-install it. Useful when an update fails.
 # See https://jira.nuxeo.com/browse/NXP-15210
 
-# Hardcode the params if you want.
-nuxeoRoot="`pwd`/../"
-studioID="liveconnect-webinar-nuxe"
+# You should pass the path to Nuxeo and the ID of the Studio project
+if [ ! -z "$1" ]
+then
+    nuxeoRoot=$1
+    studioID=$2
+else
+    echo 'You should pass the path to Nuxeo and the ID of the Studio project'
+    exit 1
+fi
 
 # Stop the server.
-./nuxeoctl stop
+$nuxeoRoot/bin/nuxeoctl stop
 
 # Remove the cached Studio project.
-rm -rf ../packages/store/$studioID-0.0.0-SNAPSHOT/
+rm -rf $nuxeoRoot/packages/store/$studioID-0.0.0-SNAPSHOT/
 
 # Remove the project JAR.
-rm -rf ../nxserver/bundles/$studioID.jar
+rm -rf $nuxeoRoot/nxserver/bundles/$studioID.jar
 
 # Install the Studio package.
-./nuxeoctl mp-install --accept true $studioID-0.0.0-SNAPSHOT
+$nuxeoRoot/bin/nuxeoctl mp-install --accept true $studioID-0.0.0-SNAPSHOT
 
 # Start the server.
-./nuxeoctl start &
+$nuxeoRoot/bin/nuxeoctl start
