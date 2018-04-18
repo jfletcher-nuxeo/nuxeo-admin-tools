1a9d7af362b8bfa60ecfe136a053aa976339ad2e
Josh Fletcher <jfletcher@nuxeo.com>
2018-04-18 11:49:13 -0700

    Update.

--- bin/reset.sh
+++ bin/reset.sh
@@ -13,17 +13,19 @@ else
     exit 1
 fi
 
+echo "WARNING: this is now imcomplete, you also need to clean '.packages' and 'pacakges.xml'"
+
 # Stop the server.
 $nuxeoRoot/bin/nuxeoctl stop
 
 # Remove the cached Studio project.
-rm -rf $nuxeoRoot/packages/store/$studioID-0.0.0-SNAPSHOT/
+rm -rf $nuxeoRoot/packages/store/$studioID*/
 
 # Remove the project JAR.
-rm -rf $nuxeoRoot/nxserver/bundles/$studioID.jar
+rm -rf $nuxeoRoot/nxserver/bundles/$studioID*.jar
 
 # Install the Studio package.
-$nuxeoRoot/bin/nuxeoctl mp-install --accept true $studioID-0.0.0-SNAPSHOT
+$nuxeoRoot/bin/nuxeoctl mp-install --accept=yes $studioID
 
 # Start the server.
 $nuxeoRoot/bin/nuxeoctl start
